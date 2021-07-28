e90096aa0883475de92d38fc841973c8b4c6c0cc
mohit1018102 <mohit10102@gmail.com>
2021-07-28 18:11:42 +0530

    intial commit

---
+++ math.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	 //Driver Code
+        //function call
+       //create add, multiply, subtract, divide
+}
---
+++ voice.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     //Driver code
+     // function call;
+     // create voice- cow, dog, cat, gun
+}
